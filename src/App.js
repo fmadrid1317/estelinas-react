2ecac982a733ea3fa7c6fc984c954bc0fdb6038a
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2022-11-01 13:52:30 -0400

    Showing Tess how the piepline works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing AWS Codepipeline by Luis Reyes Madrid. Hi Nico and Tess!
+          Hi Tess, testing!!!
         </p>
         <a
           className="App-link"
