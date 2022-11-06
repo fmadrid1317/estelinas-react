fdd740d80da18ae4daa38c887e5caf1d3f9883a0
LFRMadrid <73301821+LFRMadrid@users.noreply.github.com>
2022-11-05 22:18:44 -0400

    Showing the pipeline in action

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Tess, testing!!!
+          Testing Papo!
         </p>
         <a
           className="App-link"
